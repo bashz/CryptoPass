03e9527948fe3af06b8385fc995bcb33e0b1d139
bechir <bechir@accessnow.org>
2015-03-01 22:16:25 +0100

    Changes post Mozilla AMO audit

--- data/js/panel.js
+++ data/js/panel.js
@@ -1,12 +1,34 @@
 var container = document.getElementById("notification-list");
 self.port.on("buildNotification", function (notification, instance) {
+    if(err = document.getElementById("err")){
+        container.removeChild(err);
+    }
     var ul = document.createElement("ul");
-    var link = document.createElement("li");
+    var li = document.createElement("li");
+    li.textContent = '#' + notification.id + ' ';
+    var link = document.createElement("a");
+    link.textContent = notification.title;
+    link.setAttribute('href', instance + '/Ticket/Display.html?id=' + notification.id);
+    link.setAttribute('target', '_blanc');
     var description = document.createElement("li");
-    link.innerHTML = '#' + notification.id + ' <a target="_blanc" href="' + instance + '/Ticket/Display.html?id=' + notification.id + '"> ' + notification.title;
-    description.innerHTML = notification.description;
-    ul.appendChild(link);
+    description.textContent = notification.description;
+    li.appendChild(link);
+    ul.appendChild(li);
     ul.appendChild(description);
     container.insertBefore(ul, container.firstChild);
     document.getElementById("audio").play();
+});
+self.port.on("error", function (text, linkText, href){
+    if(err = document.getElementById("err")){
+        container.removeChild(err);
+    }
+    var span = document.createElement("span");
+    span.textContent = text;
+    span.setAttribute('id', 'err');
+    var link = document.createElement("a");
+    link.textContent = linkText;
+    link.setAttribute('href', href);
+    link.setAttribute('target', '_blanc');
+    span.appendChild(link);
+    container.appendChild(span);
 });
